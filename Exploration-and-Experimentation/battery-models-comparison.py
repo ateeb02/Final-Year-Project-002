a1a2b1be590ef760ba358e5ceec3263d4226041c
ateeb02 <ateeb.tahir.16@gmail.com>
2023-09-20 16:01:47 -0400

    compared battery models, added research papers

---
+++ Exploration-and-Experimentation/battery-models-comparison.py
@@ -0,0 +1,44 @@
+import pybamm as pb
+import numpy as np
+import matplotlib.pyplot as plt
+
+START_TIME = 0
+END_TIME = 3600
+
+battery_models = {
+    'dfn'   : pb.lithium_ion.DFN(),
+    'spm'   : pb.lithium_ion.SPM(),
+    'spme'  : pb.lithium_ion.SPMe(),
+    'mpm'   : pb.lithium_ion.MPM()
+}
+solutions = {}
+
+
+for key in battery_models:
+    print('Simulating', key, end='')
+    model = battery_models.get(key)
+    simulation = pb.Simulation(model)
+    solution = simulation.solve([START_TIME, END_TIME])
+    solutions.update({key: solution})
+    print('\t....Done')
+    print(len(solutions))
+    print(solutions[key])
+
+for key in solutions:
+    sol = solutions[key]
+
+    print('Plotting', key, end='')
+    plt.subplot(1,2,1)
+    plt.title('Terminal Voltage [V]')
+    plt.plot(sol['Terminal voltage [V]'].data, label=key)
+    
+    plt.subplot(1,2,2)
+    plt.title('Open-circuit voltage [V]')
+    plt.plot(sol['Battery open-circuit voltage [V]'].data, label=key)
+    print('\t....Done')
+
+plt.legend(loc="upper right")
+plt.show()
+
+
+print('--------Exit--------')
